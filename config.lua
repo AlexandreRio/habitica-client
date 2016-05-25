ba97d06fb83321398fd5889c75a0e420eaf1911f
Alexandre Rio <contact@alexrio.fr>
2016-05-25 23:52:05 +0200

    Initial commit

---
+++ config.lua
@@ -0,0 +1,7 @@
+local config = {}
+
+-- Find these values under https://habitica.com/#/options/settings/api
+config["user"] = ""
+config["key"]  = ""
+
+return config
---
+++ tasks.lua
@@ -0,0 +1,51 @@
+local https = require "ssl.https"
+local ltn12 = require "ltn12"
+local json  = require "json"
+
+local config = require "config"
+
+local habitica = {}
+
+function habitica.getHeaders() 
+  local headers = {}
+  headers["Content-Type"] = "application/json"
+  headers["x-api-user"] = config.user
+  headers["x-api-key"] = config.key
+  return headers
+end
+
+function habitica.request(path, method, body)
+  local headers = habitica:getHeaders()
+  local jsonbody = json.encode(body)
+
+  if (#jsonbody > 0) then
+    headers["content-length"] = #jsonbody
+    https.request{
+      url = "https://habitica.com/api/v3/" .. path,
+      method = method,
+      source = ltn12.source.string(jsonbody),
+      sink = ltn12.sink.file(io.stdout),
+      headers = headers
+    }
+  else
+    https.request{
+      url = "https://habitica.com/api/v3/" .. path,
+      method = method,
+      sink = ltn12.sink.file(io.stdout),
+      headers = headers
+    }
+  end
+end
+
+function habitica.create_todo(text)
+  local req = {}
+  req["type"] = "todo"
+  req["text"] = text
+
+  local url = "tasks/user"
+  habitica.request(url, "POST", req)
+end
+
+function habitica.list_tasks()
+  habitica.request("tasks/user", "GET", "")
+end
